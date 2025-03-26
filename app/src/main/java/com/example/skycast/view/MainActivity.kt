6cccd44cf795f8b95491e17a7bda5dab308f0532
suhailafarahat <suhailafarahat8@gmail.com>
2025-03-26 21:52:55 +0200

    Apply MVVM on Home Screen

--- app/src/main/java/com/example/skycast/view/MainActivity.kt
+++ app/src/main/java/com/example/skycast/view/MainActivity.kt
@@ -10,15 +10,15 @@ import androidx.activity.viewModels
 import androidx.compose.material3.MaterialTheme
 import com.example.skycast.data.remote.RemoteDataSource
 import com.example.skycast.data.remote.RetrofitClient
+import com.example.skycast.repository.WeatherRepository
 import com.example.skycast.viewmodel.HomeViewModel
 import com.example.skycast.viewmodel.HomeViewModelFactory
 
 class MainActivity : ComponentActivity() {
 
-    private val remoteDataSource = RemoteDataSource(RetrofitClient.apiService)
 
     private val homeViewModel: HomeViewModel by viewModels {
-        HomeViewModelFactory(application, remoteDataSource)
+        HomeViewModelFactory(application, WeatherRepository(RemoteDataSource(RetrofitClient.apiService)))
     }
 
     private val locationPermissionRequest =
--- app/src/main/java/com/example/skycast/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/example/skycast/viewModel/HomeViewModel.kt
@@ -3,13 +3,13 @@ package com.example.skycast.viewmodel
 import android.annotation.SuppressLint
 import android.app.Application
 import android.location.Location
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.example.skycast.data.model.ForecastResponse
 import com.example.skycast.data.model.WeatherResponse
-import com.example.skycast.data.remote.RemoteDataSource
 import com.example.skycast.repository.WeatherRepository
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
@@ -43,36 +43,32 @@ class HomeViewModel(application: Application, private val repository: WeatherRep
             fetchWeatherByLocation(location.latitude, location.longitude)
             fetchHourlyForecast(location.latitude, location.longitude)
         }.addOnFailureListener {
-            println("⚠️ Failed to get location: ${it.message}")
+            Toast.makeText(getApplication(), "Failed to get location: ${it.message}", Toast.LENGTH_SHORT).show()
         }
     }
 
     private fun fetchWeatherByLocation(lat: Double, lon: Double) {
         viewModelScope.launch {
-                val weather = repository.getWeather(lat, lon, API_KEY)
-                _weatherState.value = weather
-
+            val weather = repository.getWeather(lat, lon, API_KEY)
+            _weatherState.value = weather
         }
     }
 
     private fun fetchHourlyForecast(lat: Double, lon: Double) {
         viewModelScope.launch {
-                val forecast = repository.getHourlyForecast(lat, lon, API_KEY)
-                _hourlyForecast.value = forecast
-
+            val forecast = repository.getHourlyForecast(lat, lon, API_KEY)
+            _hourlyForecast.value = forecast
         }
     }
 }
 
-
-
 class HomeViewModelFactory(
     private val application: Application,
-    private val remoteDataSource: RemoteDataSource
+    private val repository: WeatherRepository
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
-            return HomeViewModel(application, WeatherRepository(remoteDataSource)) as T
+            return HomeViewModel(application, repository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
